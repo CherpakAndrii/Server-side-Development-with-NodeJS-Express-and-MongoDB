f793a935a2b6298062c20ab992cba8451690a055
CherpakAndrii <a.cherpak-2026@kpi.ua>
2022-06-29 18:01:32 +0300

    https server added

--- Express_generated_app/conFusionServer/app.js
+++ Express_generated_app/conFusionServer/app.js
@@ -11,6 +11,16 @@ var usersRouter = require('./routes/users');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var app = express();
+const uploadRouter = require('./routes/uploadRouter');
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -60,6 +70,7 @@ app.use('/', indexRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
